b6e85e98acbe084640bbac6c1f5f8ff8dd4ddd56
AvSSan <103084521+AvSSan@users.noreply.github.com>
2022-07-06 00:01:20 +1000

    Update urls.py

--- myfirst/home/urls.py
+++ myfirst/home/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('friends/', friends, name='friends'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
