8332b00fa191eb6ef5ce1dc308b1561092b6204b
AvSSan <103084521+AvSSan@users.noreply.github.com>
2022-07-05 23:30:35 +1000

    Update models.py

--- myfirst/home/models.py
+++ myfirst/home/models.py
@@ -6,4 +6,11 @@ class Friends(models.Model):
     
     
 class song(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=u"Пользователь")
+    author = models.CharField(max_length=30, blank=True, null=True, verbose_name=u"Автор")
+    song_name = models.CharField(max_length=30, blank=True, null=True, verbose_name=u"название песни")
+    lyrics = models.TextField(max_length=500, blank=True, null=True, verbose_name=u"Текст песни")
+    song_link = models.TextField(max_length=500, blank=True, null=True, verbose_name=u"Ссылка на песню(YouTube)")
+
+
     
