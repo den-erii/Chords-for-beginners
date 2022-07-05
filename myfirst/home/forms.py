9894b779bc7f6db1559c861a310cc5b33b3222d8
AvSSan <103084521+AvSSan@users.noreply.github.com>
2022-07-06 02:15:00 +1000

    Update forms.py
    Добавил форму для добавления песен

--- myfirst/home/forms.py
+++ myfirst/home/forms.py
@@ -17,3 +17,11 @@ class RegisterUserForm(UserCreationForm):
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+
+class song_form(forms.Form):
+    user = forms.CharField(label='Пользователь', max_length=100)
+    author = forms.CharField(label='Автор песни', max_length=100)
+    song_name = forms.CharField(label='Название песни', max_length=100)
+    lyrics = forms.CharField(label='Текст песни', widget=forms.Textarea)
+    song_link = forms.CharField(label='Ссылка на песню(YouTube)', widget=forms.Textarea)
